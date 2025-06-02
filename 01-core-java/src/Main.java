26917047ece79bc3a32b012d2477b09e7e7ee4e1
Saebom Park <saebom.backend@gmail.com>
2025-06-02 14:26:26 +0900

    feat: 기본 src 폴더 및 Main.java 생성

--- 01-core-java/src/Main.java
+++ 01-core-java/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Spring Dev Journey");
+        System.out.println("Hello, Spring Dev Journey!");
     }
 }
