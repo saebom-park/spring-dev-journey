af00c1655364d61ee6a8b8ae73563b971cda2a65
Saebom Park <saebom.backend@gmail.com>
2025-06-03 07:59:18 +0900

    chore: setup .gitignore for build and IDE config

--- 01-core-java/src/Main.java
+++ 01-core-java/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Spring Dev Journey!");
+        System.out.println("평생 설정만 하다가 죽겠네에에");
     }
 }
