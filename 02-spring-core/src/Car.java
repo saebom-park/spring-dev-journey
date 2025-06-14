68ec0aa1c4e42435abeda8df6c0290a0e318fb81
Saebom Park <saebom.backend@gmail.com>
2025-06-14 09:20:19 +0900

    [OOP-1단계] 클래스 vs 객체 개념 정리 및 실습 완료
    - Car.java: 클래스 설계도 기반 객체 생성 실습
    - Dog.java: 생성자 + 메서드 + 배열을 활용한 객체 생성 응용
    - oop-1.md: 클래스/객체 개념 정리 및 실습 미션 포함한 수업 문서

---
+++ 02-spring-core/src/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+    String model;
+    int speed;
+    
+    public void drive() {
+        System.out.println(model + "가 " + speed + "km/h 속도로 부릉부릉~ 달립니다!");
+    }
+    
+    public static void main(String[] args) {
+        Car myCar = new Car();  // 객체 생성
+        myCar.model = "소나타"; // 속성 설정
+        myCar.speed = 100;
+        myCar.drive();  // 메서드 호출
+    }
+}
---
+++ 02-spring-core/src/Dog.java
@@ -0,0 +1,30 @@
+public class Dog {
+    String name;
+    int age;
+
+    Dog (String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void bark() {
+        System.out.println("멍멍!");
+    }
+
+    public void introduce() {
+        System.out.println("안녕! 내 이름은 " + name + "라고 해! 나이는 귀여운 " + age + "살이야! 만나서 반가워~");
+    }
+
+    public static void main(String[] args) {
+        String[] names = {"아부", "도비"};
+        int[] ages = {7, 9};
+
+        Dog aboo = new Dog(names[0], ages[0]);
+        Dog dobi = new Dog(names[1], ages[1]);
+
+        aboo.introduce();
+        aboo.bark();
+        dobi.introduce();
+        dobi.bark();
+    }
+}
