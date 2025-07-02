175723566f9cf79d39421938ed0f1974a51d30b1
Saebom Park <saebom.backend@gmail.com>
2025-07-02 13:04:04 +0900

    feat: [SPRING-1] 초기 클래스 구조 커밋

---
+++ 02-spring-core/spring-1/src/AppConfig.java
@@ -0,0 +1,7 @@
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.spring")
+public class AppConfig {
+}
