804123e5f5a479c3685e571b40ce0229bb296645
Saebom Park <saebom.backend@gmail.com>
2025-07-09 17:54:52 +0900

    [SPRING-5단계] DTO 매핑 예시코드 작성 중간 커밋
    - OrderController.java 기본 구조 작성
    - OrderRequestDto.java 요청 DTO 클래스 정의
    - OrderResponseDto.java 응답 DTO 클래스 정의
    - SpringbootStructureApplication.java 메인 실행 구성

---
+++ 02-spring-core/spring-5/src/main/java/com/springlab11/OrderController.java
@@ -0,0 +1,7 @@
+package com.springlab11;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class OrderController {}
